7aa985db7c0d9e7471333191b39736962994afc9
Mmartz1233 <93346422+Mmartz1233@users.noreply.github.com>
2022-01-19 15:04:06 -0800

    Mayra Martz
    STEP 40

---
+++ mini_solar_sys/Assets/Scripts/RotateAround.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAround : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform tarjet;
+    public int speed;
+    void Start()
+    {
+        if (tarjet == null)
+        {
+            tarjet = this.gameObject.transform;
+            Debug.Log("RotateAround tarjet not specified. Defaulting to parent GameObject");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(tarjet.transform.position, tarjet.transform.up, speed * Time.deltaTime);
+    }
+}
